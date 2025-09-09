b07e1fbe67c40c07f9da8b3d9b47a19cc448bc73
urMatriarch <bevgbates@gmail.com>
2025-09-09 13:32:27 -0700

    0.01 First Loop is Complete :3

--- astroids-mimic/main.py
+++ astroids-mimic/main.py
@@ -1,7 +1,23 @@
 import pygame
+from constants import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             running = False
+    
+    screen.fill("black")
+    pygame.display.flip()
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
