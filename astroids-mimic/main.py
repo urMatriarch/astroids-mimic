9372597f05014a960b1ee7c41c7d0f198a9d71ed
urMatriarch <bevgbates@gmail.com>
2025-09-09 15:29:58 -0700

    0.03b I can rotate now!

--- astroids-mimic/main.py
+++ astroids-mimic/main.py
@@ -2,21 +2,24 @@ import pygame
 from constants import *
 from player import Player
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-game_clock = pygame.time.Clock()
-dt = 0
-
 def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+      
 
+        player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
--- astroids-mimic/player.py
+++ astroids-mimic/player.py
@@ -18,5 +18,17 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(self.rotation)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
 #test_player = Player(3, 4)
 #print(f"Stats are as follows: Position = {test_player.position}, radius = {test_player.radius}, rotation = {test_player.rotation}. ")
