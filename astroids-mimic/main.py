77b49855158046f0545e2427bc323c87ca6b2613
urMatriarch <bevgbates@gmail.com>
2025-09-09 13:42:36 -0700

    0.02 -> added circleshape file

--- astroids-mimic/main.py
+++ astroids-mimic/main.py
@@ -5,19 +5,19 @@ pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-             running = False
-    
-    screen.fill("black")
-    pygame.display.flip()
+game_clock = pygame.time.Clock()
+dt = 0
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = (game_clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
