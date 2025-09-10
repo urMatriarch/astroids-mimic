2fdcd59831c56e6fbc0ae431cb03e38af743d150
urMatriarch <bevgbates@gmail.com>
2025-09-10 16:43:23 -0700

    3.01 -> Asteroid Field Created!

--- astroids-mimic/main.py
+++ astroids-mimic/main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -15,6 +17,12 @@ def main():
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
